ed8c35771dca4fc23171e6fa368a777f6474e7cd
Ana Lacerda <anaclara.lacerdas@gmail.com>
2014-11-18 21:40:27 -0300

    reverting commit

--- cms/app/scripts/filters/start-from.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('cmsApp')
-  .filter('startFrom', function() {
-    return function(input, start) {
-      start = +start;
-      return (typeof input === 'object' && input.length > 0) ? input.slice(start): [];
-    };
-  });
